edf34a5c0e3be0e7b3d849eb1b6f28cd1d8aa114
ezalos <ezalos@gmail.com>
2020-07-04 18:15:03 +0200

    Open in main

---
+++ srcs/main.c
@@ -0,0 +1,17 @@
+#include "head.h"
+
+int main(int ac, char **av)
+{
+	int fd;
+
+	if (ac < 2)
+	{
+		return (EXIT_FAILURE);
+	}
+	if ((fd = open(av[1], O_RDONLY)) < 0)
+	{
+		perror("open");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT _SUCCESS);
+}
